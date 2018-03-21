199d3b9a789fa8eac3a52e69fa46f4ff3dbfface
Grady Simon <grady@gradysimon.com>
2018-03-20 21:06:08 -0700

    Add d3 hello world

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import * as dl from 'deeplearn';
+import * as d3 from 'd3';
 
 interface Environment {
 
@@ -38,14 +39,18 @@ async function runExample() {
 
   // Option 1: With async/await.
   // Caveat: in non-Chrome browsers you need to put this in an async function.
-  console.log(await result.data());  // Float32Array([3, 4, 5])
+  // console.log(await result.data());  // Float32Array([3, 4, 5])
+  let resolvedData = await result.data();
+  d3.select("body").append("span").text(resolvedData.toString());
+  // // Option 2: With a Promise.
+  // result.data().then(data => console.log(data));
 
-  // Option 2: With a Promise.
-  result.data().then(data => console.log(data));
-
-  // Option 3: Synchronous download of data.
-  // This is simpler, but blocks the UI until the GPU is done.
-  console.log(result.dataSync());
+  // // Option 3: Synchronous download of data.
+  // // This is simpler, but blocks the UI until the GPU is done.
+  // console.log(result.dataSync());
 }
 
 runExample();
+
+d3.select("body").append("span")
+    .text("Hello, world!");
