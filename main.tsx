08968da5adfa86e019a4d076b457cf24bae9ed7d
Grady Simon <grady@gradysimon.com>
2018-04-07 12:26:30 -0700

    Create d3 grid with rows/cols controlled by props

--- main.tsx
+++ main.tsx
@@ -2,6 +2,7 @@ import * as dl from 'deeplearn';
 import * as d3 from 'd3';
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { join } from 'path';
 
 interface Environment {
 
@@ -57,11 +58,25 @@ runExample();
 // TODO: factor out a D3Component superclass
 
 export type GridProps = {
+  width: number,
+  height: number,
   rows: number,
   cols: number,
+  gridCells: Array<Array<number>>
+}
+
+
+type GridCell = {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  color: string;
+  content: any;
 }
 
 export class Grid extends React.Component<GridProps, {}> {
+
   private node: SVGSVGElement;
   
   constructor(public props: GridProps) {
@@ -77,21 +92,54 @@ export class Grid extends React.Component<GridProps, {}> {
     this.callD3();
   }
 
+  private cells_(): Array<GridCell> {
+    const cellWidth = this.props.width / this.props.cols;
+    const cellHeight = this.props.height / this.props.rows;
+    let cells: Array<GridCell> = [];
+    for (let i = 0; i < this.props.rows; ++i) {
+      for (let j = 0; j < this.props.cols; j++) {
+         cells.push({
+          x: j * cellWidth,
+          y: i * cellHeight,
+          width: cellWidth,
+          height: cellHeight,
+          color: "000",
+          content: "data",
+        });
+      }
+    }
+    return cells;
+  }
+
   callD3() {
-    const node = this.node;
-    d3.select(node)
-      .selectAll('circle')
-      .data([this.props.rows, this.props.cols])
-      .enter()
-      .append('circle')
-        .attr("cx", d => d * 40)
-        .attr("cy", d => d * 40)
-        .attr("r", d => 20);
+    const gridSvg = this.node;
+    const gridCells: Array<GridCell> = this.cells_();
+
+    let cells = d3.select(this.node)
+        .selectAll(".grid-cell")
+        .data(gridCells)
+        .enter().append('rect')
+          .attr('class', 'grid-cell')
+          .attr('x', cell => cell.x)
+          .attr('y', cell => cell.y)
+          .attr('width', cell => cell.width)
+          .attr('height', cell => cell.height)
+          .attr('rx', 20)
+          .attr('ry', 20);
+
+  //   d3.select(this.node)
+  //     .selectAll('circle')
+  //     .data([this.props.rows, this.props.cols])
+  //     .enter()
+  //     .append('circle')
+  //       .attr("cx", d => d * 40)
+  //       .attr("cy", d => d * 40)
+  //       .attr("r", d => 20);
   }
 
   render() {
     return <svg ref={node => this.node = node}
-                width={500} height={500}></svg>;
+                width={this.props.width} height={this.props.height}></svg>;
   }
 }
 
@@ -100,7 +148,8 @@ export default class Demo extends React.Component {
     return (
       <>
         <h1>Hello from Demo!</h1>
-        <Grid rows={1} cols={2} />
+        <Grid width={500} height={500} rows={3} cols={3}
+              gridCells={[[1, 2], [3, 4]]}/>
       </>
     );
   }
